45300263d8d7524f3e59d56da59143b94dc2a685
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-12-05 16:20:51 +0900

    og 컴포넌트 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import Modal from '@/component/Modal';
 import AxiosInterceptor from '@/component/AxiosInterceptor';
 import Head from 'next/head';
+import Og from '@/component/Og';
 
 export default function RootLayout({
   children,
@@ -13,16 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html className="h-full">
-      <head>
-        <title>My page title</title>
-        <meta property="og:title" content="strcat" />
-        <meta
-          property="og:description"
-          content="함께 문장을 이어 롤링페이퍼를 완성해보세요! : strcat"
-        />
-        <meta property="og:url" content="https://strcat.me" />
-        <meta property="og:image" content="/App-Icon.png" />
-      </head>
+      <Og />
       <body className="h-full">
         <div className="m-auto h-full max-w-[calc(100vh*0.6)] font-sans">
           <RecoilRoot>

0485627adf6f57db3c2c3f6176157c8cc0935b6e
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-12-05 15:10:30 +0900

    og 태그 layout에  분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil';
 import './globals.css';
 import Modal from '@/component/Modal';
 import AxiosInterceptor from '@/component/AxiosInterceptor';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -12,6 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html className="h-full">
+      <Head>
+        <title>My page title</title>
+        <meta property="og:title" content="strcat" />
+        <meta
+          property="og:description"
+          content="함께 문장을 이어 롤링페이퍼를 완성해보세요! : strcat"
+        />
+        <meta property="og:url" content="https://strcat.me" />
+        <meta property="og:image" content="/App-Icon.png" />
+      </Head>
       <body className="h-full">
         <div className="m-auto h-full max-w-[calc(100vh*0.6)] font-sans">
           <RecoilRoot>
